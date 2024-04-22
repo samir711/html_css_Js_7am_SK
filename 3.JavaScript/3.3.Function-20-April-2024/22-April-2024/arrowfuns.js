99089d52380c8a43c58d6a2221604c864f7ffe55
samir711 <samir711@bitbucket.org>
2024-04-22 11:36:43 +0530

    Adding new files

---
+++ 3.JavaScript/3.3.Function-20-April-2024/22-April-2024/arrowfuns.js
@@ -0,0 +1,15 @@
+// defining arrow functions
+
+const myfun = () =>{
+                    document.write("hi, I am arrow fun");
+}
+
+// Arrow Function with params and exp return
+const add = (x, y) => { return x + y;}
+
+// Arrow function with params and implicit returns
+
+const product = (x, y) => { return x * y; }
+
+//AF with 
+
